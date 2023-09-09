2e3c22d2f93762a8095eb9e80bb331a9626bbc4d
ming-napat <nchimsumran1@nait.ca>
2023-09-09 15:42:42 -0600

    looks good

--- Test project/Assets/Scripts/TankExerciseController.cs
+++ Test project/Assets/Scripts/TankExerciseController.cs
@@ -18,13 +18,11 @@ public class TankExerciseController : MonoBehaviour
 
     [SerializeField] GameObject turret, barrel;
 
-    Vector3 angles;
-
     // Start is called before the first frame update
     void Start()
     {
         movementSpeed = 3.0f;
-        rotationSpeed = 50.0f;
+        rotationSpeed = 250.0f;
     }
 
     // Update is called once per frame
@@ -35,56 +33,50 @@ public class TankExerciseController : MonoBehaviour
         turretValue = turretAction.ReadValue<Vector2>();
         barrelValue = barrelAction.ReadValue<Vector2>();
 
-        //angles = barrel.transform.eulerAngles;
-
         turret.transform.Rotate(Vector3.up, turretValue.x * rotationSpeed * Time.deltaTime);
 
         //Rotation
-        //If the right tread is moving forward and the left is moving backward the tank should rotate to the left at full speed.sii
+        //If the right tread is moving forward and the left is moving backward the tank should rotate to the left at full speed.
         if(moveValueRight.y == 1.0f && moveValueLeft.y == -1.0f)
         {
-            transform.Rotate(Vector3.up, moveValueRight.y * rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.down, moveValueRight.y * rotationSpeed * Time.deltaTime);
         }
 
         //If the right tread is moving backward and the left is moving forward the tank should rotate to the right at full speed.
         if (moveValueRight.y == -1.0f && moveValueLeft.y == 1.0f)
         {
-            transform.Rotate(Vector3.up, moveValueRight.y * rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.down, moveValueRight.y * rotationSpeed * Time.deltaTime);
         }
 
         //If the right tread is moving forward and the left is not moving the tank should rotate to the left at half speed.
         if (moveValueRight.y == 1.0f && moveValueLeft.y == 0.0f)
         {
-            transform.Rotate(Vector3.up, moveValueRight.y * (rotationSpeed / 2) * Time.deltaTime);
+            transform.Rotate(Vector3.up, (rotationSpeed / 2) * Time.deltaTime, Space.World);
         }
 
         //If the left tread is moving forward and the right is not moving the tank should rotate to the right at half speed.
         if (moveValueRight.y == 0.0f && moveValueLeft.y == 1.0f)
         {
-            transform.Rotate(Vector3.up, 220.0f * Time.deltaTime);
+            transform.Rotate(Vector3.up, (rotationSpeed / 2) * Time.deltaTime);
         }
 
         //Reverse the rotation of the last two if the movement of the tread is backward.
-        //xxxxxxxxxxxx
+        if (moveValueRight.y == -1.0f && moveValueLeft.y == 0.0f)
+        {
+            transform.Rotate(Vector3.up, (rotationSpeed / 2) * Time.deltaTime, Space.World);
+        }
+        if (moveValueRight.y == 0.0f && moveValueLeft.y == -1.0f)
+        {
+            transform.Rotate(Vector3.down, (rotationSpeed / 2) * Time.deltaTime, Space.World);
+        }
 
-        Debug.Log(barrelValue);
         //Barrel Rotate Up – O
         //Barrel Rotate Down – L
-        barrel.transform.Rotate(Vector3.back, barrelValue.y * rotationSpeed * Time.deltaTime);
-
-        //Check the angles to see if they need to be clamped
-        //if (angles.x > 45.0f && angles.x < 180.0f)
-        //{
-        //    barrel.transform.localRotation = Quaternion.Euler(45.0f, 0, 0);
-        //}
-        //if (angles.x < 315.0f && angles.x > 180.0f)
-        //{
-        //    barrel.transform.localRotation = Quaternion.Euler(315.0f, 0, 0);
-        //}
+        barrel.transform.Rotate(Vector3.forward, barrelValue.y * rotationSpeed * Time.deltaTime);
 
         //move the object
-        transform.Translate(new Vector3(moveValueLeft.x, 0, moveValueLeft.y) * movementSpeed * Time.deltaTime);
-        transform.Translate(new Vector3(moveValueRight.x, 0, moveValueRight.y) * movementSpeed * Time.deltaTime);
+        transform.Translate(new Vector3(moveValueLeft.x, 0, moveValueLeft.y) * movementSpeed * Time.deltaTime, Space.Self);
+        transform.Translate(new Vector3(moveValueRight.x, 0, moveValueRight.y) * movementSpeed * Time.deltaTime, Space.Self);
     }
 
     private void FixedUpdate()
