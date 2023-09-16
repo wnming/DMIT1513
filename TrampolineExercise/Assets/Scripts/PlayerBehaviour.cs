7d0ce289935b14cdd7a5dc09baa90c6626a07885
ming-napat <nchimsumran1@nait.ca>
2023-09-16 00:21:18 -0600

    try and try .. still not working

--- TrampolineExercise/Assets/Scripts/PlayerBehaviour.cs
+++ TrampolineExercise/Assets/Scripts/PlayerBehaviour.cs
@@ -5,17 +5,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    [SerializeField] InputAction moveAction;
+    public InputAction moveAction;
+    Vector2 moveValue;
+    float movementSpeed, jumpSpeed;
+    bool isJumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementSpeed = 2.0f;
+        jumpSpeed = 4.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveValue = moveAction.ReadValue<Vector2>();
+
+        var keyboard = Keyboard.current;
+        if (keyboard != null)
+        {
+            if (keyboard.spaceKey.wasPressedThisFrame && !isJumping)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(0, jumpSpeed, 0);
+                isJumping = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3(moveValue.x, 0, moveValue.y) * movementSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("trampoline") || other.gameObject.CompareTag("floor"))
+        {
+            isJumping = false;
+        }
     }
 
     private void OnEnable()
--- TrampolineExercise/Assets/Scripts/TrampolineBehaviour.cs
+++ TrampolineExercise/Assets/Scripts/TrampolineBehaviour.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class TrampolineBehaviour : MonoBehaviour
 {
     //Trampoline Variables
-    [SerializeField] 
+    Rigidbody rb;
+    [SerializeField] Collider[] colliders;
+    [SerializeField] float radius;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +21,43 @@ public class TrampolineBehaviour : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        //colliders = Physics.OverlapSphere(transform.position, radius);
+        //Debug.Log(other.name);
+        colliders = Physics.OverlapBox(transform.position, transform.localScale / 2);
+        foreach (Collider collider in colliders)
+        {
+            //Debug.Log(collider.name);
+            ////float distance;
+            rb = collider.transform.gameObject.GetComponentInParent<Rigidbody>();
+            //Debug.Log("fdss");
 
-        //foreach (Collider collider in colliders)
-        //{
-        //    //float distance;
-        //    rigidbody = collider.transform.gameObject.GetComponentInParent<Rigidbody>();
+            ////distance = Vector3.Distance(transform.position, collider.transform.position);
 
-        //    //distance = Vector3.Distance(transform.position, collider.transform.position);
+            if (rb != null)
+            {
+                //Vector2 velocity = collision.relativeVelocity;
+                //rb.velocity = Vector3.Reflect(rb.GetRelativePointVelocity(collider.transform.position), Vector3.up/*make this a Vector2 pointing 90 degrees from your paddle surface*/);
 
-        //    if (rigidbody != null)
-        //    {
-        //        rigidbody.AddExplosionForce(force, transform.position, radius);
-        //    }
-        //}
+                //Debug.Log(rb.GetRelativePointVelocity(collider.transform.position));
+                //rb.AddForce(rb.GetRelativePointVelocity(collider.transform.position) * Time.deltaTime, ForceMode.VelocityChange);
+
+                Vector3 collisionNormal = (rb.transform.position - transform.position).normalized;
+                float playerCollisionSpeed = Vector3.Dot(collisionNormal, rb.velocity);
+                //rb.AddForce(rb.transform.position * playerCollisionSpeed * Time.deltaTime, ForceMode.VelocityChange);
+                rb.velocity = new Vector3(0, playerCollisionSpeed, 0);
+
+                Debug.Log(playerCollisionSpeed);
+
+                //rb.velocity += -rb.GetRelativePointVelocity(rb.transform.position);
+                //Vector3 relativeVelocity = new Vector3(0, 0, 0) - rb.velocity;
+                //relativeVelocity.y = rb.velocity.y;
+                //rb.GetRelativePointVelocity(rb.transform.position) += relativeVelocity * Time.fixedDeltaTime * 10.0f;
+                //Vector3 force = new Vector3(0.0f, 1.0f, 0.0f);
+                //rb.velocity = rb.curr
+                //1.0f * force / rb.mass;
+                //rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, collider.transform.position);
+            }
+        }
     }
 }
