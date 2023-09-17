83ae4db32936bb0f442437d9f69289eec2175bbc
ming-napat <nchimsumran1@nait.ca>
2023-09-16 23:48:53 -0600

    Done!!!!!!

--- TrampolineExercise/Assets/Scripts/TrampolineBehaviour.cs
+++ TrampolineExercise/Assets/Scripts/TrampolineBehaviour.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
 public class TrampolineBehaviour : MonoBehaviour
 {
     //Trampoline Variables
     Rigidbody rb;
-    [SerializeField] Collider[] colliders;
-    [SerializeField] float radius;
+    private float bounceRate = 1.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,46 +20,17 @@ public class TrampolineBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+ 
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        //Debug.Log(other.name);
-        colliders = Physics.OverlapBox(transform.position, transform.localScale / 2);
-        foreach (Collider collider in colliders)
+        rb = collision.transform.gameObject.GetComponentInParent<Rigidbody>();
+
+        if (rb != null)
         {
-            //Debug.Log(collider.name);
-            ////float distance;
-            rb = collider.transform.gameObject.GetComponentInParent<Rigidbody>();
-            //Debug.Log("fdss");
-
-            ////distance = Vector3.Distance(transform.position, collider.transform.position);
-
-            if (rb != null)
-            {
-                //Vector2 velocity = collision.relativeVelocity;
-                //rb.velocity = Vector3.Reflect(rb.GetRelativePointVelocity(collider.transform.position), Vector3.up/*make this a Vector2 pointing 90 degrees from your paddle surface*/);
-
-                //Debug.Log(rb.GetRelativePointVelocity(collider.transform.position));
-                //rb.AddForce(rb.GetRelativePointVelocity(collider.transform.position) * Time.deltaTime, ForceMode.VelocityChange);
-
-                Vector3 collisionNormal = (rb.transform.position - transform.position).normalized;
-                float playerCollisionSpeed = Vector3.Dot(collisionNormal, rb.velocity);
-                //rb.AddForce(rb.transform.position * playerCollisionSpeed * Time.deltaTime, ForceMode.VelocityChange);
-                rb.velocity = new Vector3(0, playerCollisionSpeed, 0);
-
-                Debug.Log(playerCollisionSpeed);
-
-                //rb.velocity += -rb.GetRelativePointVelocity(rb.transform.position);
-                //Vector3 relativeVelocity = new Vector3(0, 0, 0) - rb.velocity;
-                //relativeVelocity.y = rb.velocity.y;
-                //rb.GetRelativePointVelocity(rb.transform.position) += relativeVelocity * Time.fixedDeltaTime * 10.0f;
-                //Vector3 force = new Vector3(0.0f, 1.0f, 0.0f);
-                //rb.velocity = rb.curr
-                //1.0f * force / rb.mass;
-                //rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, collider.transform.position);
-            }
+            var speed = collision.relativeVelocity.y / bounceRate;
+            rb.velocity = new Vector3(rb.velocity.x, -speed, rb.velocity.z);
         }
     }
 }
